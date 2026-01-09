fcf1e7283b549f7e29f5935dca07308ce0c60173
Andy Newhouse <andy@techenby.com>
2026-01-09 14:03:47 -0600

    Add laravel rules

--- rector.php
+++ rector.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -22,5 +23,6 @@ return RectorConfig::configure()
         privatization: true,
         earlyReturn: true,
     )
+    ->withSetProviders(LaravelSetProvider::class)
     ->withImportNames()
     ->withPhpSets();
